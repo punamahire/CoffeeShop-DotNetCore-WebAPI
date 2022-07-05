cc8e24627a6b339349d4981fe3649f6110eca641
Punam Ahire <punambahire@gmail.com>
2022-07-04 21:46:28 -0500

    added controller and repo methods for coffee. html and js for adding and listing beans

---
+++ CoffeeShop/Repositories/CoffeeRepository.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using CoffeeShop.Models;
+
+namespace CoffeeShop.Repositories
+{
+    public class CoffeeRepository : ICoffeeRepository
+    {
+        private readonly string _connectionString;
+        public CoffeeRepository(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        private SqlConnection Connection
+        {
+            get { return new SqlConnection(_connectionString); }
+        }
+
+        public List<Coffee> GetAll()
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT Id, [Title], BeanVarietyId FROM Coffee";
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        var coffees = new List<Coffee>();
+                        while (reader.Read())
+                        {
+                            var coffee = new Coffee()
+                            {
+                                Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                                Title = reader.GetString(reader.GetOrdinal("Title")),
+                                BeanVarietyId = reader.GetInt32(reader.GetOrdinal("BeanVarietyId")),
+                            };
+                            coffees.Add(coffee);
+                        }
+
+                        return coffees;
+                    }
+                }
+            }
+        }
+
+        public Coffee Get(int id)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        SELECT c.Id, c.[Title], c.BeanVarietyId,
+                          b.Id AS 'BeanId', b.Name, b.Region
+                          FROM Coffee c
+                          LEFT JOIN BeanVariety b ON b.Id = c.BeanVarietyId
+                          WHERE c.Id = @id;";
+                    cmd.Parameters.AddWithValue("@id", id);
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        Coffee coffee = null;
+                        if (reader.Read())
+                        {
+                            coffee = new Coffee()
+                            {
+                                Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                                Title = reader.GetString(reader.GetOrdinal("Title")),
+                                BeanVarietyId = reader.GetInt32(reader.GetOrdinal("BeanVarietyId")),
+                                BeanVariety = new BeanVariety
+                                {
+                                    Id = reader.GetInt32(reader.GetOrdinal("BeanId")),
+                                    Name = reader.GetString(reader.GetOrdinal("Name")),
+                                    Region = reader.GetString(reader.GetOrdinal("Region"))
+                                }
+                            };
+                        }
+
+                        return coffee;
+                    }
+                }
+            }
+        }
+
+        public void Add(Coffee coffee)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        INSERT INTO Coffee ([Title], BeanVarietyId)
+                        OUTPUT INSERTED.ID
+                        VALUES (@title, @beanVarietyId)";
+                    cmd.Parameters.AddWithValue("@title", coffee.Title);
+                    cmd.Parameters.AddWithValue("@beanVarietyId", coffee.BeanVarietyId);
+                    if (coffee.BeanVariety == null)
+                    {
+                        cmd.Parameters.AddWithValue("@beanVariety", DBNull.Value);
+                    }
+                    else
+                    {
+                        cmd.Parameters.AddWithValue("@beanVariety", coffee.BeanVariety);
+                    }
+
+                    coffee.Id = (int)cmd.ExecuteScalar();
+                }
+            }
+        }
+
+        public void Update(Coffee coffee)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        UPDATE Coffee 
+                           SET [Title] = @title, 
+                               BeanVarietyId = @beanVarietyId 
+                         WHERE Id = @id";
+                    cmd.Parameters.AddWithValue("@id", coffee.Id);
+                    cmd.Parameters.AddWithValue("@title", coffee.Title);
+                    cmd.Parameters.AddWithValue("@beanVarietyId", coffee.BeanVarietyId);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public void Delete(int id)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "DELETE FROM Coffee WHERE Id = @id";
+                    cmd.Parameters.AddWithValue("@id", id);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
--- js-app/main.js
+++ js-app/main.js
@@ -1,5 +1,10 @@
 const url = "https://localhost:5001/api/beanvariety/";
 
+let beans = []
+const useBeans = () => {
+    return [...beans]
+}
+
 const button = document.querySelector("#run-button");
 button.addEventListener("click", () => {
     getAllBeanVarieties()
@@ -8,7 +13,113 @@ button.addEventListener("click", () => {
         })
 });
 
+const addButton = document.querySelector("#add-button");
+addButton.addEventListener("click", (event) => {
+
+    console.log("event listener");
+    CreateBean();
+});
+
+const addNewBeanVariety = beanObj => {
+    return fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(beanObj)
+    })
+        .then(response => response.json())
+        .then(parsedResponse => {
+            beans.push(parsedResponse)
+            return parsedResponse
+        })
+}
+
 function getAllBeanVarieties() {
-    return fetch(url).then(resp => resp.json());
+    return fetch(url).then(resp => resp.json())
+        .then(allBeans => {
+            const beanElement = document.querySelector(".beanList");
+            beanElement.innerHTML = BeanList(allBeans);
+        });
+}
+
+const BeanList = (allBeans) => {
+    let beanHTML = "";
+    //Loop over the array of beans and for each one, invoke the Bean component which returns HTML representation
+    for (const beanObject of allBeans) {
+        beanHTML += Bean(beanObject)
+    }
+    return beanHTML;
+
+}
+
+const Bean = (beanObject) => {
+
+    return `
+      <section class="bean">
+        <header>
+            
+            <h2 class="bean__name">${beanObject.name}</h2>
+        </header>
+        <p class="bean__region">Region: ${beanObject.region}</p>
+        <p class="bean__notes">Notes: ${beanObject.notes}</p>
+      </section>
+    `
+}
+
+const CreateBean = () => {
+    console.log("inside CreateBean");
+    document.getElementById("beanForm").innerHTML =
+        `
+        <div class="newBean">
+            <br>
+            <div>
+                <input value=""
+                    name="beanName"
+                    class="newBean__input"
+                    type="text"
+                    placeholder="Name" />
+            </div>
+            <div>
+                <input value=""
+                    name="beanRegion"
+                    class="newBean__input"
+                    type="text"
+                    placeholder="Region" />
+            </div>
+            <textarea name="beanNotes"
+                class="newBean__input newBean__Notes"
+                placeholder="Enter Notes"></textarea>
+            <button id="newBean__submit">Add</button>
+            <button id="newBean__cancel">Cancel</button>
+        </div>
+        `
+
+    // const applicationElement = document.querySelector(".coffeeshop");
+    document.getElementById("newBean__submit").addEventListener("click", event => {
+
+        console.log("inside submit event listener");
+        //collect the input values into an object to add to the DB
+        const name = document.querySelector("input[name='beanName']").value
+        const region = document.querySelector("input[name='beanRegion']").value
+        const notes = document.querySelector("textarea[name='beanNotes']").value
+
+        const beanObject = {
+            Name: name,
+            Region: region,
+            Notes: notes
+        }
+
+        // call the API method to add entry to the DB
+        addNewBeanVariety(beanObject)
+            .then(response => {
+
+                //showPostList()
+                console.log("inside api call");
+                document.querySelector("input[name='beanName']").value = ""
+                document.querySelector("input[name='beanRegion']").value = ""
+                document.querySelector("textarea[name='beanNotes']").value = ""
+            })
+    })
 }
 
